f5a992fb2c7d6c8bc02e502ceab1580f9134120a
botros <botros.nawakil@outlook.de>
2019-09-18 18:37:26 +0200

    hi

---
+++ scss/java.js
@@ -0,0 +1,45 @@
+
+    var x = document.getElementById("erste");
+    x .addEventListener("click",fun);
+    function fun(){
+      document.getElementById("reinl").innerHTML = this.innerHTML ;
+       // document.getElementByClassName("img1").innerHTML ="hsssss" 
+    }
+
+var y = document.getElementById("zwte");
+    y .addEventListener("click",fun);
+    function fun(){
+       document.getElementById("reinl").innerHTML = this.innerHTML
+    }
+
+    var z = document.getElementById("drte");
+    z .addEventListener("click",fun);
+    function fun(){
+       document.getElementById("reinl").innerHTML = this.innerHTML
+    }
+
+    var a= document.getElementById("vite");
+    a .addEventListener("click",fun);
+    function fun(){
+       document.getElementById("reinl").innerHTML = this.innerHTML
+    }
+    var b= document.getElementById("fnte");
+    b .addEventListener("click",fun);
+    function fun(){
+       document.getElementById("reinl").innerHTML = this.innerHTML
+    }
+
+
+    var c = document.getElementById("sete");
+    c .addEventListener("click",fun);
+    function fun(){
+       document.getElementById("reinl").innerHTML = this.innerHTML
+    }
+
+    var d = document.getElementById("site");
+    d .addEventListener("click",fun);
+    function fun(){
+       document.getElementById("reinl").innerHTML = this.innerHTML
+    }
+    
+    
